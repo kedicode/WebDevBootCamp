f7477e37c5d7b4076d638895d8360d97aa88fdb4
Keenan Lynch <kedicode@gmail.com>
2019-11-16 11:40:57 -0500

    Add initial Bootstrap and some styling

--- v1/app.js
+++ v1/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+//Temp Campgrounds to be pushed to db later
+const campgrounds = [
+	{name: 'Salmon Creek', image: 'https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419_960_720.jpg'},
+	{name: 'Granite Hills', image: 'https://cdn.pixabay.com/photo/2016/08/28/17/05/camping-1626412_960_720.jpg'},
+	{name: 'Mountain Goat Run', image: 'https://cdn.pixabay.com/photo/2019/01/23/13/46/camping-tipi-3950296_960_720.jpg'},
+	
+];
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
@@ -9,13 +18,7 @@ app.get('/', (req, res)=>{
 	res.render('landing');
 })
 
-app.get('/campgrounds', (req, res)=>{
-	const campgrounds = [
-		{name: 'Salmon Creek', image: 'https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419_960_720.jpg'},
-		{name: 'Granite Hills', image: 'https://cdn.pixabay.com/photo/2016/08/28/17/05/camping-1626412_960_720.jpg'},
-		{name: 'Mountain Goat Run', image: 'https://cdn.pixabay.com/photo/2019/01/23/13/46/camping-tipi-3950296_960_720.jpg'}
-	];
-	
+app.get('/campgrounds', (req, res)=>{	
 	res.render('campgrounds', {campgrounds: campgrounds});
 })
 
@@ -23,8 +26,12 @@ app.get('/campgrounds/new', (req,res)=>{
 	res.render('new');
 })
 
-app.post('/campgrounds', (reg, res)=>{
-	res.send("You Hit the post route");
+app.post('/campgrounds', (req, res)=>{
+	const name = req.body.name;
+	const image = req.body.image;
+	campgrounds.push({name:name, image: image});
+	// res.send("You Hit the post route");
+	res.redirect('/campgrounds');
 })
 
 app.listen(3000, ()=>{
