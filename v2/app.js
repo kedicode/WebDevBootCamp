06066b903b77b953ed8ba5d516dd7f0927ab236f
Keenan Lynch <kedicode@gmail.com>
2019-11-16 20:45:04 -0500

    add mongoose and initial connection

--- v2/app.js
+++ v2/app.js
@@ -1,41 +1,67 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express'),
+	bodyParser = require('body-parser'),
+	app = express(),
+	mongoose = require('mongoose');
 
-//Temp Campgrounds to be pushed to db later
-const campgrounds = [
-	{name: 'Salmon Creek', image: 'https://cdn.pixabay.com/photo/2016/01/26/23/32/camp-1163419_960_720.jpg'},
-	{name: 'Granite Hills', image: 'https://cdn.pixabay.com/photo/2016/08/28/17/05/camping-1626412_960_720.jpg'},
-	{name: 'Mountain Goat Run', image: 'https://cdn.pixabay.com/photo/2019/01/23/13/46/camping-tipi-3950296_960_720.jpg'},
-	
-];
+//Connect to Db
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://dbUser:Cw5S9sba0g7Pu8ZF0iF3@cluster0-ktf9m.mongodb.net/yelp_camp?retryWrites=true&w=majority');
 
+//Setup Schema
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
 
-app.use(bodyParser.urlencoded({extended: true}));
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create({
+// 	name: 'Granite Hills',
+// 	image: 'https://cdn.pixabay.com/photo/2016/08/28/17/05/camping-1626412_960_720.jpg'
+// }, (err, campground) => {
+// 	if (err) {
+// 		console.log(error);
+// 	} else {
+// 		console.log("Saved Campground:\n", campground);
+// 	}
+// })
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
 	res.render('landing');
 })
 
-app.get('/campgrounds', (req, res)=>{	
-	res.render('campgrounds', {campgrounds: campgrounds});
+app.get('/campgrounds', (req, res) => {
+	Campground.find({}, (err, campgrounds) => {
+		if (err) {
+			console.log(error)
+		} else {
+			res.render('campgrounds', { campgrounds: campgrounds });
+		}
+	});
 })
 
-app.get('/campgrounds/new', (req,res)=>{
+app.get('/campgrounds/new', (req, res) => {
 	res.render('new');
 })
 
-app.post('/campgrounds', (req, res)=>{
+app.post('/campgrounds', (req, res) => {
 	const name = req.body.name;
 	const image = req.body.image;
-	campgrounds.push({name:name, image: image});
+	Campground.create({ name: name, image: image }, (err, campground) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("New Campground\n", campground);
+			res.redirect('/campgrounds');
+		}
+	});
 	// res.send("You Hit the post route");
-	res.redirect('/campgrounds');
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
 	console.log("The YelpCamp Server Has Started");
 })
-
-//Testin
